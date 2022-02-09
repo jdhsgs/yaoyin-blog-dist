2d88bef03c2147b594732d6ca2540552b56d8fc2
姚寅 <991534355@qq.com>
2022-02-09 16:00:21 +0800

    取消导航

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -65,23 +65,6 @@ module.exports = {
     },
     plugins: [
         [
-            '@vuepress-reco/vuepress-plugin-kan-ban-niang', {
-                // theme: [
-                //     'miku', 'whiteCat', 'haru1', 'haru2', 'haruto', 'koharu', 'izumi', 'shizuku', 'wanko', 'blackCat', 'z16'
-                // ],
-                theme:[
-                    'shizuku'
-                ],
-                clean: true,
-                messages: {
-                    welcome: '欢迎来到我的博客', home: '心里的花，我想要带你回家。', theme: '好吧，希望你能喜欢我的其他小伙伴。', close: '你不喜欢我了吗？痴痴地望着你。'
-                },
-                messageStyle: { right: '68px', bottom: '290px' },
-                // width: 250,
-                // height: 320
-            },
-        ],
-        [
             'meting',
             {
                 // 这个 API 是不可用的，只是作为示例而已
